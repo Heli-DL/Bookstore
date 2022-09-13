23116e7b66187a93ab410a407d3e8e9b6bc2ca77
Heli-DL <heli.delorme@outlook.com>
2022-09-13 16:22:44 +0300

    Database & JPA added

--- src/main/java/s22/Bookstore/domain/Book.java
+++ src/main/java/s22/Bookstore/domain/Book.java
@@ -1,21 +1,45 @@
 package s22.Bookstore.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Book {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
 	private String title, author, isbn;
-	private int year;
+	private int bookyear;
 	private double price;
+	
+	@ManyToOne
+	@JoinColumn(name = "categoryid")
+	private Category category;
 	
 	public Book() {
 		super();
 	}
 
-	public Book(String title, String author, String isbn, int year, double price) {
+	public Book(String title, String author, String isbn, int bookyear, double price, Category category) {
 		super();
 		this.title = title;
 		this.author = author;
 		this.isbn = isbn;
-		this.year = year;
+		this.bookyear = bookyear;
 		this.price = price;
+		this.category = category;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public String getTitle() {
@@ -42,12 +66,12 @@ public class Book {
 		this.isbn = isbn;
 	}
 
-	public int getYear() {
-		return year;
+	public int getBookyear() {
+		return bookyear;
 	}
 
-	public void setYear(int year) {
-		this.year = year;
+	public void setBookyear(int bookyear) {
+		this.bookyear = bookyear;
 	}
 
 	public double getPrice() {
@@ -57,11 +81,20 @@ public class Book {
 	public void setPrice(double price) {
 		this.price = price;
 	}
+	
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
 
 	@Override
 	public String toString() {
-		return "Book [title=" + title + ", author=" + author + ", isbn=" + isbn + ", year=" + year + ", price=" + price
-				+ "]";
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", isbn=" + isbn + ", bookyear="
+				+ bookyear + ", price=" + price + ", category=" + category + "]";
 	}
+	
 	
 }
--- src/main/java/s22/Bookstore/web/BookController.java
+++ src/main/java/s22/Bookstore/web/BookController.java
@@ -1,15 +1,58 @@
 package s22.Bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import s22.Bookstore.domain.Book;
+import s22.Bookstore.domain.BookRepository;
+import s22.Bookstore.domain.CategoryRepository;
 
 @Controller
 public class BookController {
+
+	@Autowired
+	private BookRepository repository;
 	
-	@GetMapping("/index")
-	@ResponseBody
-	public String showPage() {
-		return "Bookstore";
+	@Autowired
+	private CategoryRepository crepository;
+	
+	@RequestMapping("/booklist")
+	public String bookList(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
+	}
+	
+	// Add new book
+	@RequestMapping(value = "/add")
+	public String addStudent(Model model) {
+		model.addAttribute("book", new Book());
+		model.addAttribute("categories", crepository.findAll());
+		return "addbook";
+	}
+	
+	//Edit book
+	@GetMapping("editBook/{id}")
+	public String editBook(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("editBook", repository.findById(id));
+		return "editBook";
+	}
+	
+	//Save new book
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
+	}
+	
+	//Delete book
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteStudent(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "redirect:../booklist";
 	}
 }
